6742603883563096f37d7cace5cb41b7418c9375
Sebbange <sebbang99@gmail.com>
2022-03-26 20:03:27 +0900

    Initial commit

---
+++ app/src/main/java/com/hansung/android/monthviewactivity/MyItem.java
@@ -0,0 +1,13 @@
+package com.hansung.android.monthviewactivity;
+
+public class MyItem {
+    String mClassName;
+    String mClassTime;
+    String mClassRoom;
+
+    public MyItem(String mClassName, String mClassTime, String mClassRoom) {
+        this.mClassName = mClassName;
+        this.mClassTime = mClassTime;
+        this.mClassRoom = mClassRoom;
+    }
+}
