2032711866e72d0e916160a9024ad29b3feea066
Sebbange <sebbang99@gmail.com>
2022-03-26 22:44:08 +0900

    버튼 클릭 수정

--- app/src/main/java/com/hansung/android/monthviewactivity/MainActivity.java
+++ app/src/main/java/com/hansung/android/monthviewactivity/MainActivity.java
@@ -2,8 +2,11 @@ package com.hansung.android.monthviewactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.GridView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button back = findViewById(R.id.button_back);
+        back.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+
+            }
+        });
+
+        Button next = findViewById(R.id.button_next);
+        next.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+
+            }
+        });
 
         String[] items = {"item1", "item2", "item3", "item4", "item5", "item6", "item7", "item8"};
 
