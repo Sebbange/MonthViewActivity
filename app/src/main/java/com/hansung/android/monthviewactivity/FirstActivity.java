85d39225d4f4429a37459470e057a7f64a9b7c7f
Sebbange <sebbang99@gmail.com>
2022-03-30 22:11:27 +0900

    이름 변경

--- app/src/main/java/com/hansung/android/monthviewactivity/FirstActivity.java
+++ app/src/main/java/com/hansung/android/monthviewactivity/FirstActivity.java
@@ -5,25 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.GridView;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class FirstActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_first);
 
-        Button btn = findViewById(R.id.button_next);
-        btn.setOnClickListener(new View.OnClickListener() {
+        Button btn_n = findViewById(R.id.button_first_next);
+        btn_n.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
                 startActivity(intent);
             }
         });
+
     }
 }
--- app/src/main/java/com/hansung/android/monthviewactivity/SecondActivity.java
+++ app/src/main/java/com/hansung/android/monthviewactivity/SecondActivity.java
@@ -5,17 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.GridView;
-import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_next);
+        setContentView(R.layout.activity_second);
+
+        Button btn_b = findViewById(R.id.button_second_back);
+        btn_b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), FirstActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
