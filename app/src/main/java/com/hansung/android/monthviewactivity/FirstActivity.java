3cf5191e5861e33c29f9de50a22b340d7fda8e97
Sebbange <sebbang99@gmail.com>
2022-03-30 23:02:32 +0900

    Revert "캘린더 클래스1"
    This reverts commit c54d5a146cfdbbb71eed1085c23ed53ecd3b77f8.

--- app/src/main/java/com/hansung/android/monthviewactivity/FirstActivity.java
+++ app/src/main/java/com/hansung/android/monthviewactivity/FirstActivity.java
@@ -6,10 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -17,9 +13,6 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
-
-        String date;
-        TextView textView;
 
         Button btn_n = findViewById(R.id.button_first_next);
         btn_n.setOnClickListener(new View.OnClickListener() {
@@ -29,11 +22,6 @@ public class FirstActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-        Calendar cal = Calendar.getInstance();
-        date = cal.get(Calendar.YEAR)+"년"+(cal.get(Calendar.MONTH)+1)+"월";
-        textView = (TextView)findViewById(R.id.year_first_month);
-        textView.setText(date);
 
     }
 }
--- app/src/main/java/com/hansung/android/monthviewactivity/SecondActivity.java
+++ app/src/main/java/com/hansung/android/monthviewactivity/SecondActivity.java
@@ -6,9 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-
-import java.util.Calendar;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -16,9 +13,6 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-
-        String date;
-        TextView textView;
 
         Button btn_b = findViewById(R.id.button_second_back);
         btn_b.setOnClickListener(new View.OnClickListener() {
@@ -28,11 +22,6 @@ public class SecondActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-        Calendar cal = Calendar.getInstance();
-        date = cal.get(Calendar.YEAR)+"년"+(cal.get(Calendar.MONTH)+2)+"월";
-        textView = (TextView)findViewById(R.id.year_second_month);
-        textView.setText(date);
 
     }
 }
