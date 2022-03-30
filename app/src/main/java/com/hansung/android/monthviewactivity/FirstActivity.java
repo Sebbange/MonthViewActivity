9f589cce91c2eddb674c6e9fd2df8200ce3903f6
Sebbange <sebbang99@gmail.com>
2022-03-30 23:07:40 +0900

    세번째엑티비티 추가

--- app/src/main/java/com/hansung/android/monthviewactivity/FirstActivity.java
+++ app/src/main/java/com/hansung/android/monthviewactivity/FirstActivity.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class FirstActivity extends AppCompatActivity {
