cb57e22801d84ca3d98ff6fc6e0f0a9b71d45cd7
Sebbange <sebbang99@gmail.com>
2022-04-01 20:40:45 +0900

    Revert "날짜 출력 수정"
    This reverts commit a6b445d92c18c1a3bf531d956efa9e4309c8d62c.

--- app/src/main/java/com/hansung/android/monthviewactivity/SecondActivity.java
+++ app/src/main/java/com/hansung/android/monthviewactivity/SecondActivity.java
@@ -40,38 +40,8 @@ public class SecondActivity extends AppCompatActivity {
         textView.setText(yearandmonth);
 
         ArrayList<CalendarItem> data = new ArrayList<CalendarItem>();
-        switch(cal.get(Calendar.MONTH)) {
-            case 1:
-            case 3:
-            case 5:
-            case 7:
-            case 8:
-            case 10:
-            case 12:
-                for(int i=1; i<32 ;i++) {
-                    data.add(new CalendarItem(i));
-                }
-                break;
-            case 4:
-            case 6:
-            case 9:
-            case 11:
-                for(int i=1; i<31 ;i++) {
-                    data.add(new CalendarItem(i));
-                }
-                break;
-            case 2:
-                if(cal.get(Calendar.YEAR)%4==0 && cal.get(Calendar.YEAR)%100!=0 ||
-                        cal.get(Calendar.YEAR)%400==0) {
-                    for(int i=1; i<29 ;i++) {
-                        data.add(new CalendarItem(i));
-                    }
-                } else {
-                    for(int i=1; i<28 ;i++) {
-                        data.add(new CalendarItem(i));
-                    }
-                }
-                break;
+        for(int i=1; i<(cal.getActualMaximum(Calendar.DATE))+1 ;i++) {
+            data.add(new CalendarItem(i));
         }
 
         adapter = new CalendarAdapter(this, R.layout.gridviewcalendar, data);
