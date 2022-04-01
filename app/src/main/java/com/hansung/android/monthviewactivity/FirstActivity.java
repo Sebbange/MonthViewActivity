a6b445d92c18c1a3bf531d956efa9e4309c8d62c
Sebbange <sebbang99@gmail.com>
2022-04-01 20:31:49 +0900

    날짜 출력 수정

--- app/src/main/java/com/hansung/android/monthviewactivity/FirstActivity.java
+++ app/src/main/java/com/hansung/android/monthviewactivity/FirstActivity.java
@@ -60,9 +60,39 @@ public class FirstActivity extends AppCompatActivity {
         if(firstday!=dayofweek) {
             System.out.println(" "); // 현재 출력 안됨. 매월 1일이 해당하는 요일이 아닐 때마다 공백 출력
         }
-        for(int i=1; i<(cal.getActualMaximum(Calendar.DATE))+1 ;i++) {
-            data.add(new CalendarItem(i));
-        } // 매월 1일이 요일과 일치하면 (ex: 22년 4월 1일은 금요일) 그때부터 해당 말일(4월은 30일)까지 날짜 출력
+        switch(cal.get(Calendar.MONTH)) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                for(int i=1; i<32 ;i++) {
+                    data.add(new CalendarItem(i));
+                }
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                for(int i=1; i<31 ;i++) {
+                    data.add(new CalendarItem(i));
+                }
+                break;
+            case 2:
+                if(cal.get(Calendar.YEAR)%4==0 && cal.get(Calendar.YEAR)%100!=0 ||
+                        cal.get(Calendar.YEAR)%400==0) {
+                    for(int i=1; i<29 ;i++) {
+                        data.add(new CalendarItem(i));
+                    }
+                } else {
+                    for(int i=1; i<28 ;i++) {
+                        data.add(new CalendarItem(i));
+                    }
+                }
+                break;
+        }
 
         adapter = new CalendarAdapter(this, R.layout.gridviewcalendar, data);
         GridView gridview = (GridView) findViewById(R.id.gridview);
--- app/src/main/java/com/hansung/android/monthviewactivity/SecondActivity.java
+++ app/src/main/java/com/hansung/android/monthviewactivity/SecondActivity.java
@@ -40,8 +40,38 @@ public class SecondActivity extends AppCompatActivity {
         textView.setText(yearandmonth);
 
         ArrayList<CalendarItem> data = new ArrayList<CalendarItem>();
-        for(int i=1; i<(cal.getActualMaximum(Calendar.DATE))+1 ;i++) {
-            data.add(new CalendarItem(i));
+        switch(cal.get(Calendar.MONTH)) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                for(int i=1; i<32 ;i++) {
+                    data.add(new CalendarItem(i));
+                }
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                for(int i=1; i<31 ;i++) {
+                    data.add(new CalendarItem(i));
+                }
+                break;
+            case 2:
+                if(cal.get(Calendar.YEAR)%4==0 && cal.get(Calendar.YEAR)%100!=0 ||
+                        cal.get(Calendar.YEAR)%400==0) {
+                    for(int i=1; i<29 ;i++) {
+                        data.add(new CalendarItem(i));
+                    }
+                } else {
+                    for(int i=1; i<28 ;i++) {
+                        data.add(new CalendarItem(i));
+                    }
+                }
+                break;
         }
 
         adapter = new CalendarAdapter(this, R.layout.gridviewcalendar, data);
